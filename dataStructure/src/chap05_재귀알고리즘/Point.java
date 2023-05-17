52b3e05c5cbd5fbff9ca1d127e14669304ea5436
juhee kang <mnmn092631@gmail.com>
2023-05-17 18:00:44 +0900

    Point 객체

---
+++ dataStructure/src/chap05_재귀알고리즘/Point.java
@@ -0,0 +1,39 @@
+package chap05_재귀알고리즘;
+
+class Point {
+	private int x;
+	private int y;
+
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public boolean equals(Object p) {
+		Point px = (Point)p;
+		if(this.x == px.x && this.y == px.y) return true;
+		return false;
+	}
+	
+	@Override
+	public String toString() {
+		return this.hashCode() + " : x = " + x + ", y = " + y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+}
