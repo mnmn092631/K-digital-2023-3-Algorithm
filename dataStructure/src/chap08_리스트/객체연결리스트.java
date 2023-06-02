5cdae3557001d8532bbd8e8f5b07c3ba887cbe94
juhee kang <mnmn092631@gmail.com>
2023-06-02 12:27:39 +0900

    객체연결리스트

---
+++ dataStructure/src/chap08_리스트/객체연결리스트.java
@@ -0,0 +1,217 @@
+package chap08_리스트;
+
+import java.util.Comparator;
+import java.util.Scanner;
+
+class SimpleObject {
+	static final int NO = 1; // 번호를 읽어 들일까요?
+	static final int NAME = 2; // 이름을 읽어 들일까요?
+
+	private String no; // 회원번호
+	private String name; // 이름
+
+	// --- 문자열 표현을 반환 ---//
+	public String toString() {
+		return "(" + no + ") " + name;
+	}
+
+	public SimpleObject() {
+		no = null;
+		name = null;
+	}
+
+	// --- 데이터를 읽어 들임 ---//
+	void scanData(String guide, int sw) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(guide + "할 데이터를 입력하세요.");
+
+		if ((sw & NO) == NO) { // & 는 bit 연산자임
+			System.out.print("번호: ");
+			no = sc.next();
+		}
+		if ((sw & NAME) == NAME) {
+			System.out.print("이름: ");
+			name = sc.next();
+		}
+	}
+
+	// --- 회원번호로 순서를 매기는 comparator ---//
+	public static final Comparator<SimpleObject> NO_ORDER = new NoOrderComparator();
+
+	private static class NoOrderComparator implements Comparator<SimpleObject> {
+		public int compare(SimpleObject d1, SimpleObject d2) {
+			return (d1.no.compareTo(d2.no) > 0) ? 1 : (d1.no.compareTo(d2.no) < 0) ? -1 : 0;
+		}
+	}
+
+	// --- 이름으로 순서를 매기는 comparator ---//
+	public static final Comparator<SimpleObject> NAME_ORDER = new NameOrderComparator();
+
+	private static class NameOrderComparator implements Comparator<SimpleObject> {
+		public int compare(SimpleObject d1, SimpleObject d2) {
+			return d1.name.compareTo(d2.name);
+		}
+	}
+}
+
+class Node2 {
+	SimpleObject data;
+	Node2 link;
+
+	public Node2(SimpleObject element) {
+		data = element;
+		link = null;
+	}
+}
+
+class LinkedList2 {
+	Node2 first;
+
+	public LinkedList2() {
+		first = null;
+	}
+
+	// delete the element
+	public String Delete(SimpleObject element, Comparator<SimpleObject> cc) {
+		Node2 ptr = first;
+		SimpleObject find = null;
+		if (first == null) return "List가 비어있어 삭제할 수 없습니다.";
+		if (!Search(element, cc)) return "리스트에 " + element + "번이 없습니다.";
+
+		if (cc.compare(first.data, element) == 0) {
+			find = first.data;
+			first = first.link;
+		} else {
+			while (cc.compare(ptr.link.data, element) != 0) {
+				ptr = ptr.link;
+			}
+			find = ptr.link.data;
+			ptr.link = ptr.link.link;
+		}
+		return find + "가 삭제되었습니다.";
+	}
+
+	// 전체 리스트를 순서대로 출력한다.
+	public void Show() {
+		Node2 ptr = first;
+		while (ptr != null) {
+			System.out.println(ptr.data);
+			ptr = ptr.link;
+		}
+	}
+
+	// 임의 값을 삽입할 때 리스트가 오름차순으로 정렬이 되도록 한다
+	public void Add(SimpleObject element, Comparator<SimpleObject> cc) {
+		Node2 nd = new Node2(element);
+		Node2 ptr = first;
+		Node2 pre = null;
+
+		if (first == null) {
+			first = nd;
+			return;
+		}
+		while (ptr != null) {
+			if (cc.compare(element, ptr.data) < 0) {
+				if (ptr.equals(first)) {
+					first = nd;
+					nd.link = ptr;
+					return;
+				}
+				nd.link = ptr;
+				pre.link = nd;
+				return;
+			}
+			pre = ptr;
+			ptr = ptr.link;
+		}
+		if (ptr == null) pre.link = nd;
+	}
+
+	// 검색
+	public boolean Search(SimpleObject element, Comparator<SimpleObject> cc) {
+		Node2 ptr = first;
+
+		while (ptr != null) {
+			if (cc.compare(element, ptr.data) == 0) return true;
+			ptr = ptr.link;
+		}
+		return false;
+	}
+}
+
+public class 객체연결리스트 {
+
+	enum Menu {
+		Add("삽입"), Delete("삭제"), Show("인쇄"), Search("검색"), Exit("종료");
+
+		private final String message; // 표시할 문자열
+
+		static Menu MenuAt(int idx) { // 순서가 idx번째인 열거를 반환
+			for (Menu m : Menu.values()) {
+				if (m.ordinal() == idx) return m;
+			}
+			return null;
+		}
+
+		Menu(String string) { // 생성자(constructor)
+			message = string;
+		}
+
+		String getMessage() { // 표시할 문자열을 반환
+			return message;
+		}
+	}
+
+	// --- 메뉴 선택 ---//
+	static Menu SelectMenu() {
+		Scanner sc = new Scanner(System.in);
+		int key;
+		do {
+			for (Menu m : Menu.values()) {
+				System.out.printf("(%d) %s  ", m.ordinal(), m.getMessage());
+				if ((m.ordinal() % 3) == 2 && m.ordinal() != Menu.Exit.ordinal()) System.out.println();
+			}
+			System.out.print(" : ");
+			key = sc.nextInt();
+		} while (key < Menu.Add.ordinal() || key > Menu.Exit.ordinal());
+		return Menu.MenuAt(key);
+	}
+
+	public static void main(String[] args) {
+		Menu menu; // 메뉴
+		System.out.println("Linked List");
+		LinkedList2 l = new LinkedList2();
+		Scanner sc = new Scanner(System.in);
+		SimpleObject data;
+		System.out.println("inserted");
+		l.Show();
+		do {
+			switch (menu = SelectMenu()) {
+			case Add: // 삽입
+				data = new SimpleObject();
+				data.scanData("입력", 3);
+				l.Add(data, SimpleObject.NO_ORDER);
+				break;
+			case Delete: // 삭제
+				data = new SimpleObject();
+				data.scanData("삭제", SimpleObject.NO);
+				String delResult = l.Delete(data, SimpleObject.NO_ORDER);
+				System.out.println(delResult);
+				break;
+			case Show: // 출력
+				l.Show();
+				break;
+			case Search: // 검색
+				data = new SimpleObject();
+				data.scanData("탐색", SimpleObject.NO);
+				boolean result = l.Search(data, SimpleObject.NO_ORDER);
+				if (result == false) System.out.println("검색한 데이터가 없습니다.");
+				else System.out.println("검색한 데이터가 존재합니다.");
+				break;
+			case Exit:
+				break;
+			}
+		} while (menu != Menu.Exit);
+	}
+
+}
